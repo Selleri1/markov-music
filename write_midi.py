97f5dd17b870957ddd6c83772486fc474082bd80
Elias Nilsson <elianil@ug.kth.se>
2023-12-09 19:43:05 +0100

    Change write_midi into a function

--- write_midi.py
+++ write_midi.py
@@ -1,26 +1,21 @@
 from midiutil import MIDIFile
 
+def write_notes(notes, tempo, duration, track_name, file_name):
+    mf = MIDIFile(1)
+    track = 0
+    time = 0
+    mf.addTrackName(track, time, file_name)
+    mf.addTempo(track, time, duration)
+    channel = 0
+    volume = 100
+    for i in range(len(notes)):
+        mf.addNote(track, channel, notes[i], i, duration, volume)
+    
+    with open(file_name, 'wb') as outf:
+        mf.writeFile(outf)
 
-notes = [60, 62, 64, 65, 67, 69, 71, 73]
 
-#Create MIDI object
-mf = MIDIFile(1)
 
-#Only one track
-track = 0
+notes = [40, 61, 64, 55 ,67 ,79 ,71, 63]
 
-#Start at the beginning
-time = 0
-
-mf.addTrackName(track, time, "Markov Music")
-mf.addTempo(track, time, 120)
-
-channel = 0
-volume = 100
-duration = 1
-
-for i in range(len(notes)):
-    mf.addNote(track, channel, notes[i], i, duration, volume) 
-
-with open("test.mid", 'wb') as outf:
-    mf.writeFile(outf)
+write_notes(notes, 120, 1,"Markov Music", "test.mid")  
