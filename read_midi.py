7257546efc6badd2a19a975910c3164e88a3da36
Adam Olsson <dagadamsven@gmail.com>
2023-12-09 13:59:02 +0100

    Cleanup and improve readability

--- read_midi.py
+++ read_midi.py
@@ -2,41 +2,39 @@ import midiutil.MidiFile
 import mido as m
 from enum import Enum
 
-FILENAME = "chesnuts.mid"
+FILE_PATH = "christmas_songs/"
 
 # Filnamn och vilket track som är melodin i respektive fil
 FILES = {
     "chesnuts.mid": 1,
-    "holy-night.mid": 1
+    "holy_night.mid": 1,
 }
 
-def read_midi(filename: str, track: int):
+def read_midi(filename: str, track_nr: int) -> list[int]:
+    """Öppnar midi-filen 'filename' och tar fram tonerna ur track nr 'track_nr'.
+    Returnerar en lista med tonerna som tal. T.ex. är C4 (middle-c) lika med 60."""
+    # Öppna fil
     mid = m.MidiFile(filename)
-    # for i, track in enumerate(mid.tracks):
-    #     print('Track {}: {}'.format(i, track.name))
-    #     for msg in track:
-    #         pass
-    #         #print(msg)
-    rythm_track: m.MidiTrack = mid.tracks[0]
-    note_track: m.MidiTrack = mid.tracks[1]
     
-    #tracks = []
-    #for track in mid.tracks:
     notes = []
-    for message in mid.tracks[track]:
+    
+    # Varje track består av messages. Gå igenom alla messages i track nr 'track_nr'
+    for message in mid.tracks[track_nr]:
+        # Behåll bara meddelanden av typen m.Message (de som innehåller info om vilka toner som ska spelas)
         if type(message) != m.Message:
             continue
-        note = str(message).split(" ")
         
-        print(note)
-        if note[0] == "note_on" and int(note[3][9:])==0:
-            notes.append(int(note[2][5:]))
+        note_event = message.dict()        
+        if note_event["type"] == "note_on" and note_event["velocity"] != 0:
+            note = note_event["note"]
+            notes.append(note)
             
     return notes
     
 
 def main():
-    print(read_midi(FILENAME, 1))
+    for filename, track in FILES.items():
+        print(read_midi(FILE_PATH + filename, track))
 
 if __name__ == "__main__":
     main()
