20cc7cc81fbefd0aae36a922b3e75d0f00509daa
Elias Nilsson <elianil@ug.kth.se>
2023-12-09 19:50:52 +0100

    Remove testcode and add comments

--- write_midi.py
+++ write_midi.py
@@ -1,21 +1,20 @@
 from midiutil import MIDIFile
 
 def write_notes(notes, tempo, duration, track_name, file_name):
+    """Takes a list of pitches and writes them to a file"""
     mf = MIDIFile(1)
-    track = 0
-    time = 0
+    track = 0       #Only one track
+    time = 0        #Start at 0
     mf.addTrackName(track, time, file_name)
     mf.addTempo(track, time, duration)
     channel = 0
     volume = 100
     for i in range(len(notes)):
-        mf.addNote(track, channel, notes[i], i, duration, volume)
+        mf.addNote(track, channel, notes[i], i, duration, volume)       #Adds all notes after eachother in order of list
     
-    with open(file_name, 'wb') as outf:
+    with open(file_name, 'wb') as outf:         #Writes to file 
         mf.writeFile(outf)
 
 
 
-notes = [40, 61, 64, 55 ,67 ,79 ,71, 63]
-
-write_notes(notes, 120, 1,"Markov Music", "test.mid")  
+n 
