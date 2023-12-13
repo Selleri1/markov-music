5c514b567d3ac0c5dfa84ef016cd7477d575fe89
Adam Olsson <dagadamsven@gmail.com>
2023-12-13 16:07:25 +0100

    Tonarter till låtarna

--- read_midi.py
+++ read_midi.py
@@ -7,29 +7,29 @@ FILE_PATH = "christmas_songs/"
 
 # Filnamn och vilket track som är melodin i respektive fil
 CHRISTMAS_SONGS = {
-    FILE_PATH + "chesnuts.mid": 1,
-    FILE_PATH + "holy_night.mid": 1,
-    FILE_PATH + "silent_night.mid": 0,
-    FILE_PATH + "we_wish_you.mid": 0,
-    FILE_PATH + "joy_to_the_world.mid": 2,
-    FILE_PATH + "jingle_bell.mid": 2,
-    FILE_PATH + "have_yourself.mid" : 3,
-    FILE_PATH + "deck_the_halls.mid": 1,
-    FILE_PATH + "rudolf.mid": 0,
-    FILE_PATH + "let_it_snow.mid": 0,
-    FILE_PATH + "last_christmas.mid": 0,
-    # FILE_PATH + "carol_of_the_bells": ?
-    FILE_PATH + "SantaBaby.mid": 1,
-    FILE_PATH + "ChristmasDay.mid": 2,
-    FILE_PATH + "SleighRide.mid": 5,
-    FILE_PATH + "SantaClausIsComingToTown.mid": 1,
-    FILE_PATH + "Christmas_Carols_-_White_Christmas.mid": 7,
-    FILE_PATH + "Jose_Feliciano_-_Feliz_Navidad.mid": 1,
-    FILE_PATH + "Christmas_Carols_-_Rockin_Around_The_Christmas_Tree.mid": 5,
-    FILE_PATH + "Christmas_Carols_-_Rudolph_The_Red_Nosed_Reindeer.mid": 4,
-    FILE_PATH + "Christmas_Carols_-_Little_Drummer_Boy.mid": 5,
-    FILE_PATH + "Mariah_Carey_-_All_I_Want_For_Christmas_Is_You.mid": 5,
-    FILE_PATH + "Christmas_Carols_-_The_Night_Before_Christmas.mid": 6,
+    FILE_PATH + "chesnuts.mid": (1, "F"),
+    FILE_PATH + "holy_night.mid": (1, "C"),
+    FILE_PATH + "silent_night.mid": (0, "D"),
+    FILE_PATH + "we_wish_you.mid": (0, "C"),
+    FILE_PATH + "joy_to_the_world.mid": (2, "E"),
+    FILE_PATH + "jingle_bell.mid": (2, "G"),
+    FILE_PATH + "have_yourself.mid" : (3, "G"),
+    FILE_PATH + "deck_the_halls.mid": (1, "D"),
+    FILE_PATH + "rudolf.mid": (0, "C"),
+    FILE_PATH + "let_it_snow.mid": (0, "F"),
+    FILE_PATH + "last_christmas.mid": (0, "D"),
+    # FILE_PATH + "carol_of_the_bells":( , "e")?
+    FILE_PATH + "SantaBaby.mid": (1, "F"),
+    FILE_PATH + "ChristmasDay.mid": (2, "F"),
+    FILE_PATH + "SleighRide.mid": (5, "G"),
+    FILE_PATH + "SantaClausIsComingToTown.mid": (1, "C"),
+    FILE_PATH + "Christmas_Carols_-_White_Christmas.mid": (7, "C"),
+    FILE_PATH + "Jose_Feliciano_-_Feliz_Navidad.mid": (1, "C"),
+    FILE_PATH + "Christmas_Carols_-_Rockin_Around_The_Christmas_Tree.mid": (5, "A"),
+    FILE_PATH + "Christmas_Carols_-_Rudolph_The_Red_Nosed_Reindeer.mid": (4, "C"),
+    FILE_PATH + "Christmas_Carols_-_Little_Drummer_Boy.mid": (5, "F"),
+    FILE_PATH + "Mariah_Carey_-_All_I_Want_For_Christmas_Is_You.mid": (5, "G"),
+    FILE_PATH + "Christmas_Carols_-_The_Night_Before_Christmas.mid": (6, "C"),
 }
 
 def read_midi(filename: str, track_nr: int) -> list[int]:
@@ -42,6 +42,7 @@ def read_midi(filename: str, track_nr: int) -> list[int]:
     
     # Varje track består av messages. Gå igenom alla messages i track nr 'track_nr'
     for message in mid.tracks[track_nr]:
+        print(message)
         # Behåll bara meddelanden av typen m.Message (de som innehåller info om vilka toner som ska spelas)
         if type(message) != m.Message:
             continue
@@ -53,12 +54,40 @@ def read_midi(filename: str, track_nr: int) -> list[int]:
             
     return notes
 
+def read_rythm(filename: str, track_nr: int) -> list[int]:
+    """Öppnar midi-filen 'filenam' och tar fram rytmen ur track nr 'track_nr'.
+    Returnerar en lista med duration för varje ton som ent """
+
+def find_longest_note(files: dict[str: int]) -> int:
+    for file, track_nr in files.items():
+        mid = m.MidiFile(file)
+        
+        longest = 0
+        
+        for message in mid.tracks[track_nr]:
+            if type(message) != m.Message:
+                continue
+            
+            note_event = message.dict()
+            if note_event["type"] == "note_on" and note_event["velocity"] == 0:
+                if note_event["time"] > longest:
+                    longest = note_event["time"]
+                    
+            if note_event["type"] == "note_off":
+                if note_event["time"] > longest:
+                    longest = note_event["time"]
+                    
+    return longest
+            
+
 def read_all(files: dict[str, int]) -> list[list[int]]:
     """Läser alla filer i 'files' och returnerar som en lista av listor."""
     return [read_midi(filename, track_nr) for filename, track_nr in files.items()]
 
 def main():
-    print(read_all(CHRISTMAS_SONGS))
+    #print(read_all(CHRISTMAS_SONGS))
+    #print(read_midi(FILE_PATH+"holy_night.mid", 1))
+    print(find_longest_note(CHRISTMAS_SONGS))
 
 if __name__ == "__main__":
     main()
